0341a4bed1b8c5c98c0f2fdda4bbfc28958aa728
Zdenek Kostal <zdenek.kostal@gooddata.com>
2015-03-15 14:35:50 +0100

    Move build task from gulp to npm scripts
    - Turn on progress output

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,10 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var del = require('del');
-var vinylPaths = require('vinyl-paths');
 
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 
 var webpackDevConfig = require('./webpack.dev.config.js');
-var webpackBuildConfig = require('./webpack.build.config.js');
-
-gulp.task('clean', function () {
-    return gulp
-        .src('dist', {read: false})
-        .pipe(vinylPaths(del));
-});
-
-gulp.task('webpack:build', ['clean'], function(callback) {
-    webpack(webpackBuildConfig, function(err, stats) {
-        if (err) throw new gutil.PluginError('[webpack:build]', err);
-
-        gutil.log('[webpack:build]', stats.toString({
-            colors: true
-        }));
-
-        callback();
-    });
-});
 
 gulp.task('webpack:dev', function(callback) {
     new WebpackDevServer(webpack(webpackDevConfig), {
@@ -42,5 +21,4 @@ gulp.task('webpack:dev', function(callback) {
     });
 });
 
-gulp.task('build', ['webpack:build']);
 gulp.task('default', ['webpack:dev']);
