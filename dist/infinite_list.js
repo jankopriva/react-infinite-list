5f968f28f0b0eb48f5888300602c6f35e28e0b93
Petr Benes <petr.benes@gooddata.com>
2015-05-28 15:04:36 +0200

    Avoid React warning when rendering list
    Avoid "Warning: Any use of a keyed object should be wrapped in
    React.addons.createFragment(object) before being passed as a child."

--- dist/infinite_list.js
+++ dist/infinite_list.js
@@ -46,24 +46,24 @@ module.exports = React.createClass({displayName: "exports",
         };
     },
 
-    _getListItemClass: function(item, height) {
+    _getListItemClass: function(item, height, key) {
         if (this.props.listItemClass) {
-            return React.createElement(this.props.listItemClass, {item: item, height: height});
+            return React.createElement(this.props.listItemClass, {item: item, height: height, key: key});
         }
 
-        return React.createElement(InfiniteListItem, {item: item, height: height});
+        return React.createElement(InfiniteListItem, {item: item, height: height, key: key});
     },
 
     render: function() {
-        var itemsToRender = {};
+        var itemsToRender = [];
 
-        itemsToRender['top'] = (React.createElement("div", {className: "topitem", 
-            style: {height: this.state.renderedStart * this.props.itemHeight}}));
+        itemsToRender.push(React.createElement("div", {className: "topitem", 
+            style: {height: this.state.renderedStart * this.props.itemHeight}, key: "top"}));
 
         for (var i = this.state.renderedStart; i <= this.state.renderedEnd; i++) {
             var item = this.props.items[i];
-            itemsToRender['item ' + i] = this._getListItemClass(item,
-                this.props.itemHeight);
+            itemsToRender.push(this._getListItemClass(item,
+                this.props.itemHeight, 'item ' + i));
         }
 
         return (
@@ -77,5 +77,6 @@ module.exports = React.createClass({displayName: "exports",
         );
     }
 });
+
 },{"react":"react"}]},{},[1])(1)
 });
